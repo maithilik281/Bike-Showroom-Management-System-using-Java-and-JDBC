3bd4a7c17bef588302851330e3fb7a67aecea0e3
Maithili Ajit Kamble <91479292+maithilik281@users.noreply.github.com>
2024-11-22 01:01:42 +0530

    Add files via upload

---
+++ Form2.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form2
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
